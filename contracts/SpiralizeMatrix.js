023cab7fed8cb20be6b8bb68fc213fd64fd2c7e4
Brodey Yendall <brodey_yendall@hotmail.com>
2022-01-04 12:00:25 +1100

    SpiralizeMatrix.js now succeeds when given a 1 row matrix

--- contracts/SpiralizeMatrix.js
+++ contracts/SpiralizeMatrix.js
@@ -1,70 +1,74 @@
 export function calculate(ns, contractData) {
-    let iIncrement = 0
-    let jIncrement = 1;
+    if (contractData.length > 1) {
+        let iIncrement = 0
+        let jIncrement = 1;
 
-    let result = [];
-    let visited = new Array(contractData.length);
-    for(let i = 0; i < visited.length; i++) {
-        let subArray = new Array(contractData[0].length);
-        subArray.fill(false);
-        visited[i] = subArray;
-    }
+        let result = [];
+        let visited = new Array(contractData.length);
+        // for(let i = 0; i < visited.length; i++) {
+        //     let subArray = new Array(contractData[0].length);
+        //     subArray.fill(false);
+        //     visited[i] = subArray;
+        // }
+        //
+        // prettyPrint(ns, contractData, visited);
+        // ns.tprint(visited);
 
-    // prettyPrint(ns, contractData, visited);
-    // ns.tprint(visited);
+        let i = 0;
+        let j = 0;
 
-    let i = 0;
-    let j = 0;
+        while (true) {
+            if (i >= contractData.length || j >= contractData[i].length || i < 0 || j < 0 || visited[i][j]) {
+                // go back
+                i -= iIncrement;
+                j -= jIncrement;
 
-    while(true) {
-        if(i >= contractData.length || j >= contractData[i].length || i < 0 || j < 0 || visited[i][j]) {
-            // go back
-            i -= iIncrement;
-            j -= jIncrement;
+                // change direction
+                if (iIncrement === 0 && jIncrement === 1) {
+                    iIncrement = 1;
+                    jIncrement = 0;
+                } else if (iIncrement === 1 && jIncrement === 0) {
+                    iIncrement = 0;
+                    jIncrement = (-1);
+                } else if (iIncrement === 0 && jIncrement === -1) {
+                    iIncrement = (-1);
+                    jIncrement = 0;
+                } else if (iIncrement === -1 && jIncrement === 0) {
+                    iIncrement = 0;
+                    jIncrement = 1;
+                }
 
-            // change direction
-            if(iIncrement === 0 && jIncrement === 1) {
-                iIncrement = 1;
-                jIncrement = 0;
-            } else if(iIncrement === 1 && jIncrement === 0) {
-                iIncrement = 0;
-                jIncrement = (-1);
-            } else if(iIncrement === 0 && jIncrement === -1) {
-                iIncrement = (-1);
-                jIncrement = 0;
-            } else if(iIncrement === -1 && jIncrement === 0) {
-                iIncrement = 0;
-                jIncrement = 1;
+                // go forward
+                i += iIncrement;
+                j += jIncrement;
+
+                if (visited[i][j]) {
+                    break;
+                }
             }
 
-            // go forward
+            result.push(contractData[i][j]);
+            visited[i][j] = true;
             i += iIncrement;
             j += jIncrement;
 
-            if(visited[i][j]) {
-                break;
-            }
+
+            // ns.tprint(`i: ${i}, j: ${j}, ic: ${iIncrement}, jc: ${jIncrement}`);
+            // ns.tprint(result);
+            // prettyPrint(ns, contractData, visited);
+            // await ns.sleep(500);
         }
 
-        result.push(contractData[i][j]);
-        visited[i][j] = true;
-        i += iIncrement;
-        j += jIncrement;
-
-
-        // ns.tprint(`i: ${i}, j: ${j}, ic: ${iIncrement}, jc: ${jIncrement}`);
-        // ns.tprint(result);
-        // prettyPrint(ns, data, visited);
-        // await ns.sleep(500);
+        return result;
+    } else {
+        return contractData;
     }
-
-    return result;
 }
 
 // export function prettyPrint(ns, data, visited) {
-// 	var print = "\n";
-// 	for(var i = 0; i < data.length; i++) {
-// 		for(var j = 0; j < data[i].length; j++) {
+// 	let print = "\n";
+// 	for(let i = 0; i < data.length; i++) {
+// 		for(let j = 0; j < data[i].length; j++) {
 // 			if(visited[i][j]) {
 // 				print += "--,";
 // 			} else {
